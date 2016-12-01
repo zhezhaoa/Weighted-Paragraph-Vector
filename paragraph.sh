efe75c7967715d5b379af605ea2b7522d9b6b1c4
zhezhaoa <1152543959@qq.com>
2016-12-01 20:13:10 +0800

    Update paragraph.sh

--- paragraph.sh
+++ paragraph.sh
@@ -8,7 +8,7 @@ shuf alldata-id.txt > alldata-id-shuf.txt
 
 echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 echo original paragraph vector
-time ./word2vec -train alldata-id-shuf.txt -train-pos ../data/full-train-pos.txt -train-neg ../data/full-train-neg.txt -output vectors.txt -cbow 0 -size 100 -window 10 -negative 5 -hs 0 -sample 1e-3 -threads 4 -binary 0 -iter 20 -min-count 1 -sentence-vectors 1 -nb 0
+time ./word2vec -train alldata-id-shuf.txt -train-pos ../data/full-train-pos.txt -train-neg ../data/full-train-neg.txt -output vectors.txt -cbow 0 -size 100 -window 10 -negative 5 -hs 0 -sample 1e-3 -threads 8 -binary 0 -iter 20 -min-count 1 -sentence-vectors 1 -nb 0
 grep '_\*' vectors.txt | sed -e 's/_\*//' | sort -n > sentence_vectors.txt
 
 head sentence_vectors.txt -n 25000 | awk 'BEGIN{a=0;}{if (a<12500) printf "1 "; else printf "-1 "; for (b=1; b<NF; b++) printf b ":" $(b+1) " "; print ""; a++;}' > full-train.txt
@@ -20,7 +20,7 @@ head sentence_vectors.txt -n 50000 | tail -n 25000 | awk 'BEGIN{a=0;}{if (a<1250
 #################################
 echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 echo paragraph vector with Naive bayes weighting scheme 
-time ./word2vec -train alldata-id-shuf.txt -train-pos ../data/full-train-pos.txt -train-neg ../data/full-train-neg.txt -output vectors.txt -cbow 0 -size 100 -window 10 -negative 5 -hs 0 -sample 1e-3 -threads 4 -binary 0 -iter 20 -min-count 1 -sentence-vectors 1 -nb 1
+time ./word2vec -train alldata-id-shuf.txt -train-pos ../data/full-train-pos.txt -train-neg ../data/full-train-neg.txt -output vectors.txt -cbow 0 -size 100 -window 10 -negative 5 -hs 0 -sample 1e-3 -threads 8 -binary 0 -iter 20 -min-count 1 -sentence-vectors 1 -nb 1
 grep '_\*' vectors.txt | sed -e 's/_\*//' | sort -n > sentence_vectors.txt
 
 head sentence_vectors.txt -n 25000 | awk 'BEGIN{a=0;}{if (a<12500) printf "1 "; else printf "-1 "; for (b=1; b<NF; b++) printf b ":" $(b+1) " "; print ""; a++;}' > full-train.txt
